ce69347879e4a60aab34d85110c5a767462d851e
Seriamaius <perrin.brandon45@gmail.com>
2023-01-10 10:42:44 +0100

    f

--- src/main/java/eshop/config/WebConfig.java
+++ src/main/java/eshop/config/WebConfig.java
@@ -1,26 +1,16 @@
 package eshop.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "demoMvc.controller")
+@ComponentScan(basePackages = "eshop.restcontroller")
 @Import(JpaConfig.class)
 public class WebConfig {
 	
-	@Bean
-	public UrlBasedViewResolver viewResolver() {
-		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
-		viewResolver.setViewClass(JstlView.class);
-		viewResolver.setPrefix("/WEB-INF/jsp/");
-		viewResolver.setSuffix(".jsp");
-		return viewResolver;
-	}
+
 
 }
